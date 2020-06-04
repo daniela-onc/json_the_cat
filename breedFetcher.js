759822793024a9541dbd25df9920fd5a6d274abe
Daniela <danielaoancea2009@yahoo.ca>
2020-06-04 19:10:52 +0000

    add code into js file

---
+++ breedFetcher.js
@@ -0,0 +1,20 @@
+
+const request = require('request');
+
+const fetchBreedDescription = (breedName, callback) => {
+  request.get(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(error, null);
+      } else {
+        const breed = JSON.parse(body)[0];
+        if (!breed) {
+          callback(null, "Breed not found!!!");
+        } else {
+          callback(null, breed.description);
+        }
+      }
+    }
+  );
+};
